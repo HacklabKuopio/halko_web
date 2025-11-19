b275fb3158fec7ccaf5849bed860077bed708205
jeb4 <commit@jeb4.dev>
2025-11-19 22:35:54 +0200

    Fix: remove old footer links

--- src/endpoints/seed/index.ts
+++ src/endpoints/seed/index.ts
@@ -46,7 +46,6 @@ export const seed = async ({
       payload.updateGlobal({
         slug: global,
         data: {
-          navItems: [],
         },
         depth: 0,
         context: {
@@ -302,36 +301,6 @@ export const seed = async ({
                 relationTo: 'pages',
                 value: contactPage.id,
               },
-            },
-          },
-        ],
-      },
-    }),
-    payload.updateGlobal({
-      slug: 'footer',
-      data: {
-        navItems: [
-          {
-            link: {
-              type: 'custom',
-              label: 'Admin',
-              url: '/admin',
-            },
-          },
-          {
-            link: {
-              type: 'custom',
-              label: 'Source Code',
-              newTab: true,
-              url: 'https://github.com/payloadcms/payload/tree/main/templates/website',
-            },
-          },
-          {
-            link: {
-              type: 'custom',
-              label: 'Payload',
-              newTab: true,
-              url: 'https://payloadcms.com/',
             },
           },
         ],
